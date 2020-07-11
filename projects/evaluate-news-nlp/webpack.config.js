e3d49b31554c27543fc9075763decf4c62b4f6f8
David Gouldsmith <davidgouldsmith@WTA-MBP184.local>
2020-07-11 00:26:01 -0400

    lesson 02 additions

--- projects/evaluate-news-nlp/webpack.config.js
+++ projects/evaluate-news-nlp/webpack.config.js
@@ -1,5 +1,15 @@
 const path = require("path")
 const webpack = require("webpack")
 module.exports = {
-    
+    module: {
+        rules: [
+            {
+                test: '/\.js$',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    entry: './src/client/index.js'
+
 }
